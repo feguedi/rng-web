cea4c2a82547f829061d8c9835c7264ad045cc59
Eduardo Fernández (Mac) <feguedi@yahoo.com.mx>
2021-07-28 00:29:04 -0500

    Actualizando dependencias

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict'
 require('dotenv').config({ silent: true })
-const PORT = process.env.PORT || 8010
 const express = require('express')
 const hbs = require('hbs')
 const bodyParser = require('body-parser')
@@ -9,11 +8,14 @@ const favicon = require('serve-favicon')
 const routes = require('./routes')
 const app = express()
 
+const PORT = process.env.PORT || 8010
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public'))
+
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 
